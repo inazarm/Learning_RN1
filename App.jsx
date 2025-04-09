f40e8585da11769af860b74af38ea4c3cea71132
inazarm <inazar.m@gmail.com>
2025-04-09 17:23:02 +0500

    initialization

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { View, Text,Button } from 'react-native'
+import React from 'react'
+
+// const App = () => {
+//   return (
+//   )
+// }
+
+const App2 =()=>{
+    return(
+        <View>
+            <Text style={{fontSize:30,fontWeight:'bold'}}>Thiis my new created App2 component</Text>
+            <Button title="Press"/>
+        </View>
+        
+    )
+}
+
+export default App2  

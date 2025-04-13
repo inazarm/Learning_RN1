86a342727aef50da64281c9e7c26f84e490f2023
inazarm <inazar.m@gmail.com>
2025-04-13 16:44:05 +0500

    array destructuring concept file and useState added

--- App.jsx
+++ App.jsx
@@ -1,13 +1,15 @@
 import { View, Text,Button } from 'react-native'
 import React from 'react'
 import OnPress from './src/components/OnPress'
+import UseState from './src/components/UseState'
 // import First from './src/components/first'
 
 const App = () =>{
     return(
         <View>
         {/* <Text style={{fontSize:30,fontWeight:'bold'}}>This is First component</Text> */}
-        <OnPress/>
+        {/* <OnPress/> */}
+        <UseState/>
     </View>
     )
 }
---
+++ src/components/UseState.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserState = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>UserState Hooks</Text>
+    </View>
+  )
+}
+
+export default UserState

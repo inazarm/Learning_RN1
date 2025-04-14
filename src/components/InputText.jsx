6431b42031a090baa23b60edfdb942d1e46367eb
inazarm <inazar.m@gmail.com>
2025-04-14 15:49:49 +0500

    enter in textbox and view on text field and clear both on button click

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,14 +1,15 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import React, { useState } from 'react'
 
 const InputText = () => {
-    const [userName,setUserName]=useState("");
+    const [userName,setUserName]=useState('');
   return (
     <View>
       <Text style={{fontSize:30}}>InputText</Text>
       <Text style={{fontSize:30}}>UserName: {userName}</Text>
-      <TextInput style={{fontSize:20,borderWidth:2,borderColor:'green',margin:10}} placeholder='Enter Your UserName'
+      <TextInput style={{fontSize:20,borderWidth:2,borderColor:'green',margin:10}} value={userName} placeholder='Enter Your UserName'
       onChangeText={(value) => setUserName(value)}/>
+      <Button title='Clear' onPress={()=>setUserName('')}/>
     </View>
   )
 }
